bd55621f1bc3c2c9b539838c3e103a1186451250
merallisa <merallisa@ya.ru>
2014-12-16 16:11:55 +0300

    revert

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -24,7 +24,7 @@ namespace ConsoleTest
 
             RunScheduler();
 
-            WriteLine("Main thread complete 123");
+            WriteLine("Main thread complete");
             Console.ReadKey();
         }
 
