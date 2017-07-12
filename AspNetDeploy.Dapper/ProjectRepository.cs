61c75e21fdada53dcdda4559962fbffc9df69811
Sergey <dev.sergey.popov@gmail.com>
2017-07-12 15:30:43 +0300

    ups

---
+++ AspNetDeploy.Dapper/ProjectRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetDeploy.Contracts;
+using AspNetDeploy.Model;
+using Dapper;
+
+namespace AspNetDeploy.Dapper
+{
+    public class ProjectRepository
+    {
+        private readonly DapperDataContext dataContext;
+
+        public ProjectRepository(IDataContext dataContext)
+        {
+            this.dataContext = (DapperDataContext)dataContext;
+        }
+
+        public IList<ProjectVersion> List()
+        {
+            Dictionary<int, ProjectVersion> projectVersionLookup = new Dictionary<int, ProjectVersion>();
+            this.dataContext.Connection.Query<ProjectVersion, ProjectVersionProperty, Project, ProjectVersion>(@"
+                    SELECT pv.Id Id, pv.Name Name, pv.ProjectId ProjectId, pv.SourceControlVersionId SourceControlVersionId, pv.ProjectTypeId ProjectType, pv.SolutionFile SolutionFile, pv.ProjectFile ProjectFile, pv.IsDeleted IsDeleted,
+                    pvp.Id Id, pvp.ProjectVersionId ProjectVersionId, pvp.[Key] [Key], pvp.[Value] [Value],
+                    p.*
+                    FROM ProjectVersion pv
+                    INNER JOIN ProjectVersionProperty pvp ON pv.Id = pvp.ProjectVersionId
+                    INNER JOIN SourceControlVersion scv ON pv.SourceControlVersionId = scv.Id
+                    INNER JOIN Project p ON pv.ProjectId = p.Id
+                    WHERE scv.IsArchivedId != 2
+                ", (pv, pvp, p) =>
+            {
+                ProjectVersion projectVersion;
+
+                if (!projectVersionLookup.TryGetValue(pv.Id, out projectVersion))
+                {
+                    projectVersionLookup.Add(pv.Id, projectVersion = pv);
+                }
+
+                if (pv.Properties == null)
+                {
+                    projectVersion.Properties = new List<ProjectVersionProperty>();
+                }
+
+                pv.Project = p;
+
+                projectVersion.Properties.Add(pvp);
+
+                return pv;
+            }).AsQueryable();
+
+            return projectVersionLookup.Values.ToList();
+        }
+    }
+}
---
+++ AspNetDeploy.Dapper/SourceRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetDeploy.Contracts;
+using AspNetDeploy.Model;
+using Dapper;
+
+namespace AspNetDeploy.Dapper
+{
+    public class SourceRepository
+    {
+        private readonly DapperDataContext dataContext;
+
+        public SourceRepository(IDataContext dataContext)
+        {
+            this.dataContext = (DapperDataContext)dataContext;
+        }
+
+        public IList<SourceControl> List()
+        {
+            Dictionary<int, SourceControl> sourceControlLookup = new Dictionary<int, SourceControl>();
+            this.dataContext.Connection.Query<SourceControl, SourceControlVersion, SourceControl>(@"
+                    SELECT sc.Id Id, sc.Name Name, sc.TypeId Type, sc.IsDeleted IsDeleted, sc.OrderIndex OrderIndex, 
+                    scv.Id Id, scv.SourceControlId SourceControlId, scv.ParentVersionId ParentVersionId, scv.Name Name, scv.OrderIndex OrderIndex, scv.IsHead IsHead, scv.IsArchivedId ArchiveState
+                    FROM SourceControl sc
+                    INNER JOIN SourceControlVersion scv ON sc.Id = scv.SourceControlId
+                    WHERE scv.IsArchivedId != 2
+                ", (sc, scv) =>
+            {
+                SourceControl sourceControl;
+                if (!sourceControlLookup.TryGetValue(sc.Id, out sourceControl))
+                {
+                    sourceControlLookup.Add(sc.Id, sourceControl = sc);
+                }
+
+                if (sc.SourceControlVersions == null)
+                {
+                    sourceControl.SourceControlVersions = new List<SourceControlVersion>();
+                }
+
+                sourceControl.SourceControlVersions.Add(scv);
+
+                return sc;
+            }).AsQueryable();
+
+            return sourceControlLookup.Values.ToList();
+        }
+    }
+}
