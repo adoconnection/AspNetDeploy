0772cc93c8e76ad9389ca30c8408e0c425e40b63
adoconnection <adoconnection@gmail.com>
2018-02-27 18:07:57 +0300

    json configs

--- SatelliteService/Operations/ConfigOperation.cs
+++ SatelliteService/Operations/ConfigOperation.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Xml;
 using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
 using SatelliteService.Contracts;
 using SatelliteService.Helpers;
 
@@ -25,16 +27,33 @@ namespace SatelliteService.Operations
         {
             this.backupConfigGuid = this.BackupRepository.StoreFile((string)configuration.file);
 
-            XmlDocument target = new XmlDocument();
-            target.Load((string)configuration.file);
+            if (this.IsXml((string) configuration.file))
+            {
+                XmlDocument target = new XmlDocument();
+                target.Load((string)configuration.file);
 
-            XmlDocument source = new XmlDocument();
-            source.LoadXml((string)configuration.content);
+                XmlDocument source = new XmlDocument();
+                source.LoadXml((string)configuration.content);
 
-            XmlMerger xmlMerger = new XmlMerger(target, new Dictionary<string, object>());
-            xmlMerger.ApplyChanges(source);
+                XmlMerger xmlMerger = new XmlMerger(target, new Dictionary<string, object>());
+                xmlMerger.ApplyChanges(source);
 
-            target.Save((string)configuration.file);
+                target.Save((string)configuration.file);
+            }
+            else
+            {
+                JObject target = JObject.Parse(File.ReadAllText((string)this.configuration.file));
+                JObject source = JObject.Parse((string)configuration.content);
+
+                JsonMergeSettings jsonMergeSettings = new JsonMergeSettings
+                {
+                    MergeArrayHandling = MergeArrayHandling.Replace
+                };
+
+                target.Merge(source, jsonMergeSettings);
+
+                File.WriteAllText((string)configuration.file, target.ToString());
+            }
         }
 
         public override void Rollback()
@@ -43,6 +62,20 @@ namespace SatelliteService.Operations
             {
                 this.BackupRepository.RestoreFile(this.backupConfigGuid.Value);
             }
+        }
+
+        private bool IsXml(string text)
+        {
+            try
+            {
+                XDocument xDocument = XDocument.Load(text);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
